1e261a659f72f8093d9073280e9f30f4a260878e
Michael Ishola <myckhel1@hotmail.com>
2020-07-30 18:55:04 +0100

    Fix for method not found error on linux

--- src/Facades/Vtpass.php
+++ src/Facades/Vtpass.php
@@ -4,8 +4,14 @@ namespace Myckhel\Vtpass\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+
+use Myckhel\Vtpass\Request;
+use Myckhel\Vtpass\Traits\HasQuery;
+
 class Vtpass extends Facade
 {
+    use Request, HasQuery;
+
     /**
      * Get the registered name of the component.
      *
